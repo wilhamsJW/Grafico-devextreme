2546b3107c3bc8db02033f9ea44244be4bc5fb16
Wilhams Junior <47488525+wilhamsJW@users.noreply.github.com>
2024-09-30 17:54:06 -0300

    Create comFuncaoQueCAlculaMilhoes.js

---
+++ comFuncaoQueCAlculaMilhoes.js
@@ -0,0 +1,37 @@
+export const dataSource = [
+  {
+    year: 100, // Exemplo de ano representando 100k
+    country: "Brazil",
+    money: 300,
+  },
+  {
+    year: 200,
+    country: "USA",
+    money: 400,
+  },
+  {
+    year: 300,
+    country: "Germany",
+    money: 500,
+  },
+  {
+    year: 400,
+    country: "Japan",
+    money: 600,
+  },
+  {
+    year: 500,
+    country: "China",
+    money: 800,
+  },
+  {
+    year: 1000,
+    country: "Paraíba",
+    money: 1000,
+  },
+  {
+    year: 6000,
+    country: "aíba",
+    money: 4000,
+  },
+];
