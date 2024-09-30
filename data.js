66d58f3c18bd71450861b12063cec396c606e543
Wilhams Junior <47488525+wilhamsJW@users.noreply.github.com>
2024-09-30 15:05:33 -0300

    Create data.js

---
+++ data.js
@@ -0,0 +1,32 @@
+export const dataSource = [
+  {
+    year: 100, // Exemplo de ano representando 100k
+    country: "Brazil",
+    money: 300,
+  },
+  {
+    year: 200,
+    country: "USA",
+    money: 400,
+  },
+  {
+    year: 300,
+    country: "Germany",
+    money: 500,
+  },
+  {
+    year: 400,
+    country: "Japan",
+    money: 600,
+  },
+  {
+    year: 500,
+    country: "China",
+    money: 800,
+  },
+  {
+    year: 1000,
+    country: "Paraíba",
+    money: 1000,
+  },
+];
